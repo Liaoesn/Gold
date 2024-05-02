6176211d2638f96981a3486ce1d9beb855db8a76
FranciscoCHANG <francisco940513@gmail.com>
2024-05-02 11:33:14 +0800

    沒寫完的installment list

---
+++ services/installment/app.py
@@ -0,0 +1,58 @@
+# 匯入Blueprint模組
+from flask import request, render_template
+from flask import Blueprint
+import os
+import uuid
+import psycopg2
+
+from utils import db, common
+# from flask_login import login_required
+
+# 產生產品服務藍圖
+installment_bp = Blueprint('installment_bp', __name__)
+
+#--------------------------
+# 在產品服務藍圖加入路由
+#--------------------------
+#產品清單
+@installment_bp.route('/list')
+# @login_required
+def installment_list(): 
+    page = request.args.get('page', 1, type=int)
+    per_page = 8
+
+    # 計算偏移量
+    offset = (page - 1) * per_page
+
+     #取得資料庫連線 
+    connection = db.get_connection() 
+    cursor = connection.cursor()  
+    
+    cursor.execute('SELECT installment.*, sales.soldno FROM installment INNER JOIN sales ON installment.soldno = sales.soldno order BY installment.soldno')
+    
+    total_data_count = cursor.fetchone()[0]
+
+    total_pages = (total_data_count + per_page - 1) // per_page
+
+    if page < 1:
+        page = 1
+    elif page > total_pages:
+        page = total_pages
+
+    cursor.execute('''
+        SELECT installment.*, sales.soldno 
+        FROM installment 
+        INNER JOIN sales ON installment.soldno = sales.soldno 
+        order BY installment.soldno 
+        LIMIT %s OFFSET %s
+    ''', (per_page, offset))
+    
+    #取出資料
+    data = cursor.fetchall()    
+    print(data)
+    #關閉資料庫連線    
+    connection.close() 
+    
+    #渲染網頁  
+    return render_template('installment/list.html', data=data, page=page, per_page=per_page, total_data_count=total_data_count,total_pages=total_pages)
+
